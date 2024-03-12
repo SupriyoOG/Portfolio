51f398b5a0647c1483457d67dd1db468aaf1ed24
SupriyoOG <supriyoduttakps007@gmail.com>
2024-03-12 15:46:41 +0530

    update

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const projectBtn = document.getElementById('projectBtn');
 
 
 projectBtn.addEventListener('mouseover', () => {
-        info.style.transform = 'translateY(-1800%)';
+        info.style.transform = 'translateY(0)';
 });
 
 projectBtn.addEventListener('mouseout', () => {
