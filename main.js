8bcdc3042ceb4068d7806f55e44cb9f31722e7cd
SupriyoOG <supriyoduttakps007@gmail.com>
2024-03-14 21:17:49 +0530

    update

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ const projectBtn = document.getElementById('projectBtn');
 
 
 projectBtn.addEventListener('mouseover', () => {
-        info.style.transform = 'translateY(0)';
+        info.style.transform = 'translate(0)';
 });
 
 projectBtn.addEventListener('mouseout', () => {
-        info.style.transform = 'translateY(-500vh)';
+        info.style.transform = 'translate(-100vh) rotate(60deg) skew(50deg)';
 });
